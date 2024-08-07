456a9bd00ea04e93e08e30d91574bebed6603022
subhransujena2468@gmail.com <subhransujena2468@gmail.com>
2024-08-07 22:24:31 +0530

    day 1 - start of the project news app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import NewsItems from './components/NewsItems';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar/>
+         <div className='container my-3'>
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className='row'>
+          <div className='col-md-4'>
+            <NewsItems title="mytitle" description="mydesc"/>
+          </div>
+          <div className='col-md-4'>
+            <NewsItems title="mytitle" description="mydesc"/>
+          </div>
+          <div className='col-md-4'>
+            <NewsItems title="mytitle" description="mydesc"/>
+          </div>
+        </div>
+        <div className='row mt-4'>
+          <div className='col-md-4'>
+            <NewsItems title="mytitle" description="mydesc"/>
+          </div>
+          <div className='col-md-4'>
+            <NewsItems title="mytitle" description="mydesc"/>
+          </div>
+          <div className='col-md-4'>
+            <NewsItems title="mytitle" description="mydesc"/>
+          </div>
+
+        </div>
+        <News/>
+        
+        
+      </div>
+      </>
+     
+    )
+  }
 }
-
-export default App;
---
+++ src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a news componnet.
+        
+      </div>
+    )
+  }
+}
+
+export default News
