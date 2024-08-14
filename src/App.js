ba79aef76de65da3d802776e80745fc6218b6de6
subhransujena2468@gmail.com <subhransujena2468@gmail.com>
2024-08-15 00:18:08 +0530

    create the news cards to show the current news items.

--- src/App.js
+++ src/App.js
@@ -2,42 +2,14 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import NewsItems from './components/NewsItems';
+// import NewsItems from './components/NewsItems';
 
 export default class App extends Component {
   render() {
     return (
       <>
         <Navbar/>
-         <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
-        <div className='row'>
-          <div className='col-md-4'>
-            <NewsItems title="mytitle" description="mydesc"/>
-          </div>
-          <div className='col-md-4'>
-            <NewsItems title="mytitle" description="mydesc"/>
-          </div>
-          <div className='col-md-4'>
-            <NewsItems title="mytitle" description="mydesc"/>
-          </div>
-        </div>
-        <div className='row mt-4'>
-          <div className='col-md-4'>
-            <NewsItems title="mytitle" description="mydesc"/>
-          </div>
-          <div className='col-md-4'>
-            <NewsItems title="mytitle" description="mydesc"/>
-          </div>
-          <div className='col-md-4'>
-            <NewsItems title="mytitle" description="mydesc"/>
-          </div>
-
-        </div>
         <News/>
-        
-        
-      </div>
       </>
      
     )
